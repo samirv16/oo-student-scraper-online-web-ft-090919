aedfd13c677cf65d36c4e80d54b58488fb42b2e4
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-30 14:07:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     students = []
     html = Nokogiri::HTML(open(index_url))
-    index.css("div.student-card").each do |student|
+    html.css("div.student-card").collect do |student|
       student_details = {
       :name = student.css("h4.student-name").text
       :location = student.css("p.student-location").text
