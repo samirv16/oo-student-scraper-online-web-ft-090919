6bd1334c091e339fcbc645606c2305545c9b19f6
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-30 14:19:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,7 +8,8 @@ class Scraper
     doc = Nokogiri::HTML(html)
     student_cards = doc.css(".student-card a")
     student_cards.collect do |element|
-      {:name => element.css(".student-name").text ,
+      {
+        :name => element.css(".student-name").text ,
         :location => element.css(".student-location").text,
         :profile_url => element.attr('href')
       }
@@ -22,16 +23,3 @@ class Scraper
 
 end
 
-
-# students = []
-#     html = Nokogiri::HTML(open(index_url))
-#     html.css(".student-card").collect do |student|
-#       student_details = {
-#       name: student.css(".student-name").text
-#       location: student.css(".student-location").text
-#       profile_url: "http://students.learn.co/" + sutudent.css("a").attribute("href")
-#       # student.attr("href")
-#       }
-#       students << student_details
-#     end
-#     students
