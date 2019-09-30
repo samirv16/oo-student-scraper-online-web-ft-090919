87cb9eb64cf583564675d1bebc4640ca10d0faae
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-30 13:53:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,12 +8,14 @@ class Scraper
     html = open(index_url)
     index = Nokogiri::HTML(html)
     index.css("div.student-card").each do |student|
-      student_details = {}
-      student_details[:name] = student.css("h4.student-name").text
-      student_details[:location] = student.css("p.student-location").text
+      student_details = {
+      :name = student.css("h4.student-name").text
+      :location = student.css("p.student-location").text
       profile_path = student.css("a").attribute("href").value
-      student_details[:profile_url] = './fixtures/student-site/' + profile_path
+      :profile_url = './fixtures/student-site/' + profile_path
+      }
       students << student_details
+      
     end
     students
   end
