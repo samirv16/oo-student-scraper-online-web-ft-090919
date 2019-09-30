45c0a3c4dc4b3dc9801ced9816d24521ac20334c
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-30 14:16:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,18 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    students = []
-    html = Nokogiri::HTML(open(index_url))
-    html.css(".student-card").collect do |student|
-      student_details = {
-      name: student.css(".student-name").text
-      location: student.css(".student-location").text
-      profile_url: "http://students.learn.co/" + sutudent.css("a").attribute("href")
-      # student.attr("href")
-      }
-      students << student_details
-    end
-    students
+    
   end
 
   def self.scrape_profile_page(profile_url)
@@ -23,3 +12,17 @@ class Scraper
     
 
 end
+
+
+# students = []
+#     html = Nokogiri::HTML(open(index_url))
+#     html.css(".student-card").collect do |student|
+#       student_details = {
+#       name: student.css(".student-name").text
+#       location: student.css(".student-location").text
+#       profile_url: "http://students.learn.co/" + sutudent.css("a").attribute("href")
+#       # student.attr("href")
+#       }
+#       students << student_details
+#     end
+#     students
